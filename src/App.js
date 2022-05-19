51d168ebb0e4bdcba621eb77dd7c262f3c3a309b
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-05-19 18:13:25 +0530

    components done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
+import Navbar from './components/Navbar';
 import { Box, Stack} from '@mui/material';
 function App() {
   return (
     <Box>
-      {/* <Navbar/> */}
-      <Stack direction="row" spacing={2} justifyContent="center">
-        <Sidebar/>
+      <Navbar/>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar/> 
         <Feed/>
         <Rightbar/>
       </Stack>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Hiking } from '@mui/icons-material';
+import { AppBar, Toolbar, styled, Typography } from '@mui/material';
+import React from 'react';
 
+const StyledToolbar = styled(Toolbar)({
+  display:"flex",
+  justifyContent:"space-between"
+}) 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant='h5' sx={{display:{xs:"none", sm:"block"}}}>
+          Tib DEV
+
+        </Typography>
+        <Hiking sx={{display:{xs:"block", sm:"none" , color:"black"}}} fontSize="large" />
+      </StyledToolbar>
+    </AppBar>
   )
 }
 
